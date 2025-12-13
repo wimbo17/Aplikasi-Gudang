4f9beef864709ef67a074e50943a82af8b7372f0
aldikurniawan19 <aldikurniawann15@gmail.com>
2025-12-13 20:55:40 +0700

    merubah route ke tampilan login

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 
 Auth::routes();
