3208af46d660810ce120372a83eb927d83403b4f
wimbo17 <Wimbosedayu93@gmail.com>
2025-12-13 19:14:32 +0700

    menambahkan template

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,11 +16,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        User::create([
+            'name' => env('USER_NAME'),
+            'email' => env('USER_EMAIL'),
+            'password' => Hash::make(env('USER_PASSWORD')),
+
         ]);
     }
 }
